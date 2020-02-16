754e955beb5291c426875aac099bdbab847e02fa
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-02-16 17:39:05 +0900

    Update translation_zh_CN.ts

--- translation_zh_CN.ts
+++ translation_zh_CN.ts
@@ -4,87 +4,87 @@
     <context>
         <message>
             <source>Cartesian Grid for NaysEddy x64</source>
-            <translation></translation>
+            <translation>笛卡尔网格 NaysEddy x64</translation>
         </message>
         <message>
             <source>Grid</source>
-            <translation></translation>
+            <translation>网格</translation>
         </message>
         <message>
             <source>Geometry</source>
-            <translation></translation>
+            <translation>几何</translation>
         </message>
         <message>
             <source>Bed topography</source>
-            <translation></translation>
+            <translation>床形</translation>
         </message>
         <message>
             <source>Flatbed</source>
-            <translation></translation>
+            <translation>平坦</translation>
         </message>
         <message>
             <source>2D-sine</source>
-            <translation></translation>
+            <translation>2D-正弦</translation>
         </message>
         <message>
             <source>2D-Dune cosine</source>
-            <translation></translation>
+            <translation>2D-沙丘余弦</translation>
         </message>
         <message>
             <source>2D-Dune triangle</source>
-            <translation></translation>
+            <translation>2D沙丘三角形</translation>
         </message>
         <message>
             <source>Custom</source>
-            <translation></translation>
+            <translation>自定义</translation>
         </message>
         <message>
             <source>Bedform length [m]</source>
-            <translation></translation>
+            <translation>床长 [m]</translation>
         </message>
         <message>
             <source>Bedform width [m]</source>
-            <translation></translation>
+            <translation>床宽[m]</translation>
         </message>
         <message>
             <source>Bedform height [m]</source>
-            <translation></translation>
+            <translation>床高 [m]</translation>
         </message>
         <message>
             <source>Angle of lee-side [deg]</source>
-            <translation></translation>
+            <translation>背侧角度[度]</translation>
         </message>
         <message>
             <source>Number of bedforms</source>
-            <translation></translation>
+            <translation>床个数</translation>
         </message>
         <message>
             <source>Bed topography file</source>
-            <translation></translation>
+            <translation>床形文件</translation>
         </message>
         <message>
             <source>Grid points</source>
-            <translation></translation>
+            <translation>网格点</translation>
         </message>
         <message>
             <source>Minimum x [m]</source>
-            <translation></translation>
+            <translation>最小x [m]</translation>
         </message>
         <message>
             <source>Minimum y [m]</source>
-            <translation></translation>
+            <translation>最小y [m]</translation>
         </message>
         <message>
             <source>Zero level [m]</source>
-            <translation></translation>
+            <translation>零位[m]</translation>
         </message>
         <message>
             <source>nx (imax=2^nx)</source>
-            <translation></translation>
+            <translation>nx (i最大=2^nx)</translation>
         </message>
         <message>
             <source>ny (jmax=2^ny)</source>
-            <translation></translation>
+            <translation>ny (j最大=2^ny)</translation>
         </message>
     </context>
 </TS>
