3a0c28d8a5a208a7d9e7fe57ca49935485ee3021
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-02-16 17:21:38 +0900

    Add files via upload

---
+++ translation_zh_CN.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="zh_CN">
+    <!---->
+    <context>
+        <message>
+            <source>Cartesian Grid for NaysEddy x64</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Grid</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Geometry</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Bed topography</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Flatbed</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>2D-sine</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>2D-Dune cosine</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>2D-Dune triangle</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Custom</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Bedform length [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Bedform width [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Bedform height [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Angle of lee-side [deg]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Number of bedforms</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Bed topography file</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Grid points</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Minimum x [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Minimum y [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Zero level [m]</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>nx (imax=2^nx)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>ny (jmax=2^ny)</source>
+            <translation></translation>
+        </message>
+    </context>
+</TS>
